b169ba76b3ec281446466ce518d7006f1a99921b
Elisa Andrade <elisalvsan@gmail.com>
2022-06-09 12:39:35 -0300

    update

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,12 +2,16 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      colors:{
-        brand:{
+      colors: {
+        brand: {
+          300: '#996DFF',
           500: '#8257E5',
         }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
